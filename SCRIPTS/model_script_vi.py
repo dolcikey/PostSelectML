b67bbf3e7091e61af0448f837a435b95f644bda4
dolcikey <dolcikey@gmail.com>
2020-09-29 03:43:10 +0000

    updated models in AWS

--- SCRIPTS/model_script_vi.py
+++ SCRIPTS/model_script_vi.py
@@ -18,7 +18,8 @@ INPUT_TENSOR_NAME = "inputs_input" # According to Amazon, needs to match the nam
 def keras_model_fn(hyperparameters):
     model = Sequential()
 
-    model.add(Conv2D(32, kernel_size=(3, 3), input_shape=(HEIGHT, WIDTH, DEPTH), activation="relu", name="inputs",padding="same"))
+    model.add(Conv2D(32, kernel_size=(3, 3), input_shape=(HEIGHT, WIDTH, DEPTH), 
+                     activation="relu",name="inputs",padding="same"))
     model.add(MaxPooling2D())
 
     model.add(Conv2D(128, kernel_size=(3, 3), activation="relu", padding="same"))
@@ -74,90 +75,3 @@ def _input(mode, batch_size, data_dir):
 
 # This script is based off the script created by Paul Breton (Medium Article: Keras in the cloud with Amazon SageMaker)
 
-
-def _call_model_fn_eval_distributed(self, input_fn, config):
-    """Call model_fn in distribution mode and handle return values."""
-
-    iterator, input_hooks = self._get_iterator_from_input_fn(
-        input_fn, model_fn_lib.ModeKeys.EVAL, self._eval_distribution)
-
-    is_tpu_strategy = (
-        self._eval_distribution.__class__.__name__ == 'TPUStrategy')
-
-    if is_tpu_strategy:
-      steps_per_run_variable = training.get_or_create_steps_per_run_variable()
-      def step_fn(ctx, inputs):
-        """Runs one step of the eval computation and captures outputs."""
-        if isinstance(inputs, tuple):
-          features, labels = inputs
-        else:
-          features = inputs
-          labels = None
-        estimator_spec = self._eval_distribution.extended.call_for_each_replica(
-            self._call_model_fn,
-            args=(features, labels, model_fn_lib.ModeKeys.EVAL, config))
-        eval_metric_ops = _verify_and_create_loss_metric(
-            estimator_spec.eval_metric_ops, estimator_spec.loss,
-            self._eval_distribution)
-        update_op, eval_dict = _extract_metric_update_ops(
-            eval_metric_ops, self._eval_distribution)
-        ctx.set_non_tensor_output(name='estimator_spec', output=estimator_spec)
-        ctx.set_non_tensor_output(name='eval_dict', output=eval_dict)
-        return update_op
-
-      # TODO(priyag): Fix eval step hook to account for steps_per_run.
-      ctx = self._eval_distribution.extended.experimental_run_steps_on_iterator(
-          step_fn, iterator, iterations=steps_per_run_variable)
-      update_op = ctx.run_op
-      eval_dict = ctx.non_tensor_outputs['eval_dict']
-      grouped_estimator_spec = ctx.non_tensor_outputs['estimator_spec']
-    else:
-      features, labels = estimator_util.parse_iterator_result(
-          iterator.get_next())
-      grouped_estimator_spec = (
-          self._eval_distribution.extended.call_for_each_replica(
-              self._call_model_fn,
-              args=(features, labels, model_fn_lib.ModeKeys.EVAL, config)))
-      eval_metric_ops = _verify_and_create_loss_metric(
-          grouped_estimator_spec.eval_metric_ops, grouped_estimator_spec.loss,
-          self._eval_distribution)
-      update_op, eval_dict = _extract_metric_update_ops(
-          eval_metric_ops, self._eval_distribution)
-
-    scaffold = _combine_distributed_scaffold(
-        grouped_estimator_spec.scaffold, self._eval_distribution)
-    evaluation_hooks = self._eval_distribution.unwrap(
-        grouped_estimator_spec.evaluation_hooks)[0]
-    evaluation_hooks = evaluation_hooks + (
-        estimator_util.StrategyInitFinalizeHook(
-            self._eval_distribution.initialize,
-            self._eval_distribution.finalize),)
-
-    return (scaffold, evaluation_hooks, input_hooks, update_op, eval_dict)
-
-  def _evaluate_run(self, checkpoint_path, scaffold, update_op, eval_dict,
-                    all_hooks, output_dir):
-    """Run evaluation."""
-    eval_results = evaluation._evaluate_once(  # pylint: disable=protected-access
-        checkpoint_path=checkpoint_path,
-        master=self._config.evaluation_master,
-        scaffold=scaffold,
-        eval_ops=update_op,
-        final_ops=eval_dict,
-        hooks=all_hooks,
-        config=self._session_config)
-
-    current_global_step = eval_results[ops.GraphKeys.GLOBAL_STEP]
-
-    _write_dict_to_summary(
-        output_dir=output_dir,
-        dictionary=eval_results,
-        current_global_step=current_global_step)
-
-    if checkpoint_path:
-      _write_checkpoint_path_to_summary(
-          output_dir=output_dir,
-          checkpoint_path=checkpoint_path,
-          current_global_step=current_global_step)
-
-    return eval_results
